fa81c08f6c77f9e99421dd8a4250add962009d61
Justin Klaußnitzer <jstnklnr021@gmail.com>
2022-03-27 22:30:58 +0200

    Forgot to push change in cut.command.ts

--- src/commands/cut.command.ts
+++ src/commands/cut.command.ts
@@ -3,7 +3,7 @@ import { SFVimEditor, SFVimMode } from "../types/SFVimEditor";
 import { copyRange, getLeftPosition, getRelativePosition, RelativeDirection, selectionToRange } from "../utilities/selection.util";
 import { executeModeChangeVisual } from "./modeVisual.command";
 
-export function executeCutMoveBefore(vimEditor: SFVimEditor, amplifier: number) {
+export function executeCut(vimEditor: SFVimEditor, amplifier: number) {
     if(amplifier != 0 || !(vimEditor.mode & SFVimMode.VISUAL)) {
         return;
     }
